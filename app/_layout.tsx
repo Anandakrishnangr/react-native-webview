0b326b60b3c24b76d0024234ae8b455aded21e11
anandakrishnan <granandakrishnan@gmail.com>
2024-12-29 19:31:17 +0530

    first

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,8 +5,11 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
+import * as SystemUI from 'expo-system-ui';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import TabTwoScreen from './(tabs)/explore';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import changeNavigationBarColor from 'react-native-navigation-bar-color';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -21,6 +24,7 @@ export default function RootLayout() {
     if (loaded) {
       SplashScreen.hideAsync();
     }
+    SystemUI.setBackgroundColorAsync('#75428c');
   }, [loaded]);
 
   if (!loaded) {
@@ -28,12 +32,15 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1, backgroundColor: colorScheme === 'dark' ? '#000' : '#fff' }}>
+        <StatusBar
+          backgroundColor={colorScheme === 'dark' ? '#000' : '#fff'}
+        />
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <TabTwoScreen />
+        </ThemeProvider>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
